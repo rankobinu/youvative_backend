c9d0cfe47f26fb5178ad205eb48b7014e9a2b4d8
rankobinu <boss2822004@gmail.com>
2025-05-27 15:10:37 +0100

    Prepared backend for deployment

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+error_log("Requested Path: " . $path);
 require_once 'config/database.php';
 require_once 'utils/helpers.php';
 
