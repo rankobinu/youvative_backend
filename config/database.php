5932f3581e51c3ca51d4e90bcadac82aa6bd11a1
rankobinu <boss2822004@gmail.com>
2025-05-27 13:20:09 +0100

    Prepared backend for deployment

--- config/database.php
+++ config/database.php
@@ -1,16 +1,23 @@
 <?php
 class Database {
-    private $host = 'localhost';
-    private $db_name = 'youva_project';
-    private $username = 'root';
-    private $password = '';
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
     private $conn;
+
+    public function __construct() {
+        // Get values from environment variables
+        $this->host = getenv('DB_HOST');
+        $this->db_name = getenv('DB_NAME');
+        $this->username = getenv('DB_USER');
+        $this->password = getenv('DB_PASS');
+    }
 
     public function getConnection() {
         $this->conn = null;
         try {
-            // Use PDO to connect to the database
-            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo "Connection Error: " . $e->getMessage();
