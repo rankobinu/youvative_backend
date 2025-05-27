987ef9db672caeb93f98069068071aceb8d528a8
rankobinu <boss2822004@gmail.com>
2025-05-27 14:54:13 +0100

    Prepared backend for deployment

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
 // index.php
-
+// Enable CORS for frontend access
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 require_once 'config/database.php';
 require_once 'utils/helpers.php';
+
+// Handle preflight requests
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 // Get the request path
 $request_uri = $_SERVER['REQUEST_URI'];
