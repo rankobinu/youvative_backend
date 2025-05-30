c6436f606d9811b10402bbdb912ba26429035610
rankobinu <boss2822004@gmail.com>
2025-05-30 22:45:52 +0100

    update

--- api/auth.php
+++ api/auth.php
@@ -3,6 +3,8 @@ header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../controllers/AuthController.php';
