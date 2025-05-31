a153e74aeb3907d58d021c1f9475aa75a9848a40
rankobinu <boss2822004@gmail.com>
2025-05-31 14:43:29 +0100

    update

--- index.php
+++ index.php
@@ -44,7 +44,9 @@ if (strpos($path, 'api/auth') === 0) {
     $_GET['endpoint'] = $endpoint;
     require_once 'api/strategy.php';
 
-} elseif (strpos($path, 'api/dashboard') === 0) {
+} elseif (strpos($path, 'api/admin/dashboard') === 0) {
+    $endpoint = substr($path, strlen('api/admin/dashboard/'));
+    $_GET['endpoint'] = $endpoint;
     require_once 'api/admin/dashboard.php';
 
 } else {
